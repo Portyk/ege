a6cfbae3a71fae5bb3582db89c2f5fccf13f82b5
Port <106401154+Portyk@users.noreply.github.com>
2022-08-17 23:47:36 +0300

    Add files via upload

---
+++ exe_2.py
@@ -0,0 +1,8 @@
+print('x y z w')
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                F = (your expression) #be careful with brackets
+                if F == 0 or 1: #look at the task
+                    print(x y z w)
